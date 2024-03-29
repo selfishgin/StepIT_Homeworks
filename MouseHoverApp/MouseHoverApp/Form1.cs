8557793e1e81a7bfb417af7809012af9a5542277
selfishgin <78321534+selfishgin@users.noreply.github.com>
2024-03-29 04:39:19 +0400

    Mouse Hover  - Button Location Changes

---
+++ MouseHoverApp/MouseHoverApp/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace MouseHoverApp
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Size = new System.Drawing.Size(900, 600);
+        }
+
+
+        private void button1_MouseHover(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            int newX = random.Next(0, this.ClientSize.Width - button.Width);
+            int newY = random.Next(0, this.ClientSize.Height - button.Height);
+            button.Location = new Point(newX, newY);
+        }
+
+    }
+}
---
+++ MouseHoverApp/MouseHoverApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MouseHoverApp
+{
+    internal class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Application.EnableVisualStyles(); // Enable visual styles for the application
+            Application.SetCompatibleTextRenderingDefault(false); // Set compatibility with older versions of Windows Forms
+
+            // Create and show your Windows Forms application
+
+
+            Application.Run(new Form1()); // Start the application message loop
+
+
+        }
+    }
+}
