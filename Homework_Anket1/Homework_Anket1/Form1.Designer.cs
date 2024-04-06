a23d973561cbcd890295e51a21dd84b67ce0da90
selfishgin <78321534+selfishgin@users.noreply.github.com>
2024-04-07 01:45:25 +0400

    Anket1

---
+++ Homework_Anket1/Homework_Anket1/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace Homework_Anket1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AdText = new System.Windows.Forms.TextBox();
+            this.SoyadText = new System.Windows.Forms.TextBox();
+            this.EmailText = new System.Windows.Forms.TextBox();
+            this.TelefonText = new System.Windows.Forms.TextBox();
+            this.DoğumTarixiBox = new System.Windows.Forms.DateTimePicker();
+            this.Ad = new System.Windows.Forms.Label();
+            this.Soyad = new System.Windows.Forms.Label();
+            this.Email = new System.Windows.Forms.Label();
+            this.Telefon = new System.Windows.Forms.Label();
+            this.DoğumTarixi = new System.Windows.Forms.Label();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.ListBox1 = new System.Windows.Forms.Label();
+            this.Dəyiş = new System.Windows.Forms.Button();
+            this.ƏlavəEt = new System.Windows.Forms.Button();
+            this.Load = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            this.FaylAdı = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // AdText
+            // 
+            this.AdText.Location = new System.Drawing.Point(108, 37);
+            this.AdText.Name = "AdText";
+            this.AdText.Size = new System.Drawing.Size(200, 20);
+            this.AdText.TabIndex = 0;
+            // 
+            // SoyadText
+            // 
+            this.SoyadText.Location = new System.Drawing.Point(108, 77);
+            this.SoyadText.Name = "SoyadText";
+            this.SoyadText.Size = new System.Drawing.Size(200, 20);
+            this.SoyadText.TabIndex = 1;
+            // 
+            // EmailText
+            // 
+            this.EmailText.Location = new System.Drawing.Point(108, 119);
+            this.EmailText.Name = "EmailText";
+            this.EmailText.Size = new System.Drawing.Size(200, 20);
+            this.EmailText.TabIndex = 2;
+            // 
+            // TelefonText
+            // 
+            this.TelefonText.Location = new System.Drawing.Point(108, 161);
+            this.TelefonText.Name = "TelefonText";
+            this.TelefonText.Size = new System.Drawing.Size(200, 20);
+            this.TelefonText.TabIndex = 3;
+            // 
+            // DoğumTarixiBox
+            // 
+            this.DoğumTarixiBox.Location = new System.Drawing.Point(108, 216);
+            this.DoğumTarixiBox.Name = "DoğumTarixiBox";
+            this.DoğumTarixiBox.Size = new System.Drawing.Size(200, 20);
+            this.DoğumTarixiBox.TabIndex = 4;
+            // 
+            // Ad
+            // 
+            this.Ad.AutoSize = true;
+            this.Ad.Location = new System.Drawing.Point(23, 40);
+            this.Ad.Name = "Ad";
+            this.Ad.Size = new System.Drawing.Size(20, 13);
+            this.Ad.TabIndex = 5;
+            this.Ad.Text = "Ad";
+            // 
+            // Soyad
+            // 
+            this.Soyad.AutoSize = true;
+            this.Soyad.Location = new System.Drawing.Point(23, 80);
+            this.Soyad.Name = "Soyad";
+            this.Soyad.Size = new System.Drawing.Size(37, 13);
+            this.Soyad.TabIndex = 6;
+            this.Soyad.Text = "Soyad";
+            // 
+            // Email
+            // 
+            this.Email.AutoSize = true;
+            this.Email.Location = new System.Drawing.Point(23, 122);
+            this.Email.Name = "Email";
+            this.Email.Size = new System.Drawing.Size(32, 13);
+            this.Email.TabIndex = 7;
+            this.Email.Text = "Email";
+            // 
+            // Telefon
+            // 
+            this.Telefon.AutoSize = true;
+            this.Telefon.Location = new System.Drawing.Point(23, 164);
+            this.Telefon.Name = "Telefon";
+            this.Telefon.Size = new System.Drawing.Size(43, 13);
+            this.Telefon.TabIndex = 8;
+            this.Telefon.Text = "Telefon";
+            // 
+            // DoğumTarixi
+            // 
+            this.DoğumTarixi.AutoSize = true;
+            this.DoğumTarixi.Location = new System.Drawing.Point(23, 222);
+            this.DoğumTarixi.Name = "DoğumTarixi";
+            this.DoğumTarixi.Size = new System.Drawing.Size(66, 13);
+            this.DoğumTarixi.TabIndex = 9;
+            this.DoğumTarixi.Text = "DoğumTarixi";
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(366, 37);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(282, 199);
+            this.listBox.TabIndex = 10;
+            // 
+            // ListBox1
+            // 
+            this.ListBox1.AutoSize = true;
+            this.ListBox1.Location = new System.Drawing.Point(493, 21);
+            this.ListBox1.Name = "ListBox1";
+            this.ListBox1.Size = new System.Drawing.Size(40, 13);
+            this.ListBox1.TabIndex = 11;
+            this.ListBox1.Text = "Listbox";
+            // 
+            // Dəyiş
+            // 
+            this.Dəyiş.Location = new System.Drawing.Point(233, 260);
+            this.Dəyiş.Name = "Dəyiş";
+            this.Dəyiş.Size = new System.Drawing.Size(75, 23);
+            this.Dəyiş.TabIndex = 12;
+            this.Dəyiş.Text = "Dəyiş";
+            this.Dəyiş.UseVisualStyleBackColor = true;
+            this.Dəyiş.Click += new System.EventHandler(this.Dəyiş_Click);
+            // 
+            // ƏlavəEt
+            // 
+            this.ƏlavəEt.Location = new System.Drawing.Point(233, 289);
+            this.ƏlavəEt.Name = "ƏlavəEt";
+            this.ƏlavəEt.Size = new System.Drawing.Size(75, 23);
+            this.ƏlavəEt.TabIndex = 13;
+            this.ƏlavəEt.Text = "Əlavə et";
+            this.ƏlavəEt.UseVisualStyleBackColor = true;
+            this.ƏlavəEt.Click += new System.EventHandler(this.ƏlavəEt_Click);
+            // 
+            // Load
+            // 
+            this.Load.Location = new System.Drawing.Point(366, 289);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(75, 23);
+            this.Load.TabIndex = 14;
+            this.Load.Text = "Load";
+            this.Load.UseVisualStyleBackColor = true;
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(573, 289);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 15;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+
+            // 
+            // FaylAdı
+            // 
+            this.FaylAdı.Location = new System.Drawing.Point(366, 260);
+            this.FaylAdı.Name = "FaylAdı";
+            this.FaylAdı.Size = new System.Drawing.Size(282, 20);
+            this.FaylAdı.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(685, 335);
+            this.Controls.Add(this.FaylAdı);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Load);
+            this.Controls.Add(this.ƏlavəEt);
+            this.Controls.Add(this.Dəyiş);
+            this.Controls.Add(this.ListBox1);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.DoğumTarixi);
+            this.Controls.Add(this.Telefon);
+            this.Controls.Add(this.Email);
+            this.Controls.Add(this.Soyad);
+            this.Controls.Add(this.Ad);
+            this.Controls.Add(this.DoğumTarixiBox);
+            this.Controls.Add(this.TelefonText);
+            this.Controls.Add(this.EmailText);
+            this.Controls.Add(this.SoyadText);
+            this.Controls.Add(this.AdText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox AdText;
+        private System.Windows.Forms.TextBox SoyadText;
+        private System.Windows.Forms.TextBox EmailText;
+        private System.Windows.Forms.TextBox TelefonText;
+        private System.Windows.Forms.DateTimePicker DoğumTarixiBox;
+        private System.Windows.Forms.Label Ad;
+        private System.Windows.Forms.Label Soyad;
+        private System.Windows.Forms.Label Email;
+        private System.Windows.Forms.Label Telefon;
+        private System.Windows.Forms.Label DoğumTarixi;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Label ListBox1;
+        private System.Windows.Forms.Button Dəyiş;
+        private System.Windows.Forms.Button ƏlavəEt;
+        private System.Windows.Forms.Button Load;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.TextBox FaylAdı;
+    }
+}
+
---
+++ Homework_Anket1/Homework_Anket1/Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+
+namespace Homework_Anket1
+{
+    public partial class Form1 : Form
+    {
+        private List<SurveyItem> surveyItems = new List<SurveyItem>();
+        private const string defaultFileName = "survey.json";
+        private string currentFileName = defaultFileName;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadSurveyData(currentFileName);
+        }
+
+        private void ƏlavəEt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(AdText.Text) ||
+                string.IsNullOrWhiteSpace(SoyadText.Text) ||
+                string.IsNullOrWhiteSpace(EmailText.Text) ||
+                string.IsNullOrWhiteSpace(TelefonText.Text) ||
+                string.IsNullOrWhiteSpace(DoğumTarixiBox.Text))
+            {
+                MessageBox.Show("Please fill in all fields.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            var newSurveyItem = new SurveyItem
+            {
+                Ad = AdText.Text,
+                Soyad = SoyadText.Text,
+                Email = EmailText.Text,
+                Telefon = TelefonText.Text,
+                DoğumTarixi = DoğumTarixiBox.Value
+            };
+
+            listBox.Items.Add(newSurveyItem);
+
+            surveyItems.Add(newSurveyItem);
+
+            ClearFields();
+        }
+
+        private void ClearFields()
+        {
+            AdText.Clear();
+            SoyadText.Clear();
+            EmailText.Clear();
+            TelefonText.Clear();
+            DoğumTarixiBox.Value = DateTime.Now;
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            SaveSurveyData();
+        }
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+            currentFileName = Ad.Text.Trim();
+            LoadSurveyData(currentFileName);
+        }
+
+        private void SaveSurveyData()
+        {
+            string fileName = FaylAdı.Text.Trim();
+
+            if (string.IsNullOrWhiteSpace(fileName))
+                fileName = defaultFileName;
+
+            List<SurveyItem> existingData = new List<SurveyItem>();
+            if (File.Exists(fileName))
+            {
+                string existingJsonData = File.ReadAllText(fileName);
+                existingData = JsonConvert.DeserializeObject<List<SurveyItem>>(existingJsonData);
+            }
+
+            existingData.AddRange(surveyItems);
+
+            string jsonData = JsonConvert.SerializeObject(existingData);
+
+            File.WriteAllText(fileName, jsonData);
+
+            MessageBox.Show("Survey information saved successfully.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+
+        private void LoadSurveyData(string fileName)
+        {
+            if (File.Exists(fileName))
+            {
+                string jsonData = File.ReadAllText(fileName);
+                surveyItems = JsonConvert.DeserializeObject<List<SurveyItem>>(jsonData);
+
+                SurveyItem selectedItem = surveyItems.Find(item => item.Ad == FaylAdı.Text);
+
+                if (selectedItem != null)
+                {
+                    AdText.Text = selectedItem.Ad;
+                    SoyadText.Text = selectedItem.Soyad;
+                    EmailText.Text = selectedItem.Email;
+                    TelefonText.Text = selectedItem.Telefon;
+                    DoğumTarixiBox.Value = selectedItem.DoğumTarixi;
+
+                    MessageBox.Show("Survey information loaded successfully.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("No survey information found for the specified name.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show("No saved survey information found for the specified file.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void Dəyiş_Click(object sender, EventArgs e)
+        {
+            if (listBox.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select an item to update.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            SurveyItem selectedSurveyItem = (SurveyItem)listBox.SelectedItem;
+            selectedSurveyItem.Ad = AdText.Text;
+            selectedSurveyItem.Soyad = SoyadText.Text;
+            selectedSurveyItem.Email = EmailText.Text;
+            selectedSurveyItem.Telefon = TelefonText.Text;
+            selectedSurveyItem.DoğumTarixi = DoğumTarixiBox.Value;
+
+            listBox.Items[listBox.SelectedIndex] = selectedSurveyItem;
+
+            surveyItems[listBox.SelectedIndex] = selectedSurveyItem;
+
+            MessageBox.Show("Survey information updated successfully.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void listBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox.SelectedIndex != -1)
+            {
+                SurveyItem selectedSurveyItem = (SurveyItem)listBox.SelectedItem;
+                AdText.Text = selectedSurveyItem.Ad;
+                SoyadText.Text = selectedSurveyItem.Soyad;
+                EmailText.Text = selectedSurveyItem.Email;
+                TelefonText.Text = selectedSurveyItem.Telefon;
+                DoğumTarixiBox.Value = selectedSurveyItem.DoğumTarixi;
+            }
+        }
+
+
+    }
+
+    public class SurveyItem
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Email { get; set; }
+        public string Telefon { get; set; }
+        public DateTime DoğumTarixi { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Ad}";
+        }
+    }
+}
